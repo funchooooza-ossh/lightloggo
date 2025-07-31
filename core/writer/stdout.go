8f2201953f78e104b6fc194ac65494dcf0d8faa7
funchooooza <uroddaunich@gmail.com>
2025-07-31 12:07:00 +1000

    add: Flush interface add: comments

--- core/writer/stdout.go
+++ core/writer/stdout.go
@@ -4,14 +4,21 @@ import (
 	"os"
 )
 
+// StdoutWriter пишет логи в стандартный вывод.
 type StdoutWriter struct{}
 
+// NewStdoutWriter создаёт StdoutWriter.
 func NewStdoutWriter() *StdoutWriter {
 	return &StdoutWriter{}
 }
 
+// Write выводит отформатированные данные в stdout, добавляя перенос строки.
 func (w *StdoutWriter) Write(data []byte) error {
-	// Простой println с newline
 	_, err := os.Stdout.Write(append(data, '\n'))
 	return err
+}
+
+// Flush реализует интерфейс Flushable, но ничего не делает (stdout не буферизуется).
+func (w *StdoutWriter) Flush() error {
+	return nil
 }
