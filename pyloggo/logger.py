583317e2518c8b09a57543a29125e972993f2a26
funchooooza <uroddaunich@gmail.com>
2025-08-02 15:03:32 +1000

    add:support for scope and traceback, ability to set the level for traceback output todo: ability to turn scope and traceback on and off completely, configurable maximum traceback depth

--- pyloggo/logger.py
+++ pyloggo/logger.py
@@ -2,14 +2,19 @@ from .ffi.ffi import lib
 import json
 from .route import RouteProcessor
 from .c import CLogger
+import sys
+import linecache
+import os
+from .enums import LogLevel
 
 import threading
 
 
 class Logger:
-    def __init__(self, routes: list["RouteProcessor"]):
+    def __init__(self, routes: list["RouteProcessor"], tb_level: int = 50):
         route_ids = [r.id for r in routes]
         self._c_logger = CLogger(route_ids)
+        self._tb_level = tb_level
 
     @property
     def id(self) -> int:
@@ -17,10 +22,49 @@ class Logger:
 
     def _log(self, method: str, msg: str, **kwargs):
         msg_b = msg.encode()
+
+        if LogLevel._from_string(method.capitalize()) >= self._tb_level:
+            kwargs["tb"] = self._add_traceback()
+        else:
+            kwargs["scope"] = self._add_scope()
+
         fields_b = json.dumps(kwargs or {}).encode()
         getattr(lib, f"Logger_{method.capitalize()}WithFields")(
             self.id, msg_b, fields_b
         )
+
+    def _add_scope(self, frame_depth: int = 4) -> str:
+        try:
+            frame = sys._getframe(frame_depth)
+            filename = os.path.basename(frame.f_code.co_filename)
+            lineno = frame.f_lineno
+            func = frame.f_code.co_name
+            return f"{filename}:{lineno} in {func}()"
+        except Exception:
+            return "<scope unavailable>"
+
+    def _add_traceback(self, max_depth: int = 10, skip: int = 4) -> str:
+        lines = []
+        frame = sys._getframe(skip)
+
+        for _ in range(max_depth):
+            if frame is None:
+                break
+
+            filename_full = frame.f_code.co_filename
+            filename = os.path.basename(filename_full)
+            lineno = frame.f_lineno
+            func = frame.f_code.co_name
+
+            code_line = linecache.getline(filename_full, lineno).strip()
+
+            lines.append(
+                f'  File "{filename}", line {lineno}, in {func}()\n    {code_line}\n'
+            )
+
+            frame = frame.f_back
+
+        return "".join(lines)
 
     def trace(self, msg: str, **kwargs):
         self._log("trace", msg, **kwargs)
@@ -94,10 +138,10 @@ class GlobalLogger:
             self._logger.close()
             self._logger = self._create_default_logger()
 
-    def configure(self, routes: list):
+    def configure(self, routes: list, tb_level: LogLevel = 50):
         with self._lock:
             self._logger.close()
-            self._logger = Logger(routes=list(routes))
+            self._logger = Logger(routes=list(routes), tb_level=tb_level)
 
     def close(self):
         with self._lock:
