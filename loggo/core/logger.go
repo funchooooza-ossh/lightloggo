719d7cf04592586e73a57e895278a7bde9648503
funchooooza <funchoooozaossh@mail.com>
2025-08-09 22:32:15 +1000

    upd: sacrifice logger api to make next step

--- loggo/core/logger.go
+++ loggo/core/logger.go
@@ -3,7 +3,6 @@ package core
 import (
 	"context"
 	"sync"
-	"time"
 )
 
 // Logger управляет маршрутизацией логов и жизненным циклом воркеров.
@@ -39,51 +38,4 @@ func (l *Logger) Close() {
 	}
 	l.cancel()
 	l.wg.Wait()
-}
-
-// log формирует LogRecord и отправляет его в подходящие маршруты.
-func (l *Logger) log(level LogLevel, msg string, fields map[string]interface{}) {
-
-	record := LogRecord{
-		Level:     level,
-		Timestamp: time.Now(),
-		Message:   msg,
-		Fields:    fields,
-	}
-
-	for _, route := range l.routes {
-		if route.ShouldLog(record.Level) {
-			route.Enqueue(record)
-		}
-	}
-}
-
-// Trace отправляет TRACE-сообщение.
-func (l *Logger) Trace(msg string, fields map[string]interface{}) {
-	l.log(Trace, msg, fields)
-}
-
-// Debug отправляет DEBUG-сообщение.
-func (l *Logger) Debug(msg string, fields map[string]interface{}) {
-	l.log(Debug, msg, fields)
-}
-
-// Info отправляет INFO-сообщение.
-func (l *Logger) Info(msg string, fields map[string]interface{}) {
-	l.log(Info, msg, fields)
-}
-
-// Warn отправляет WARNING-сообщение.
-func (l *Logger) Warn(msg string, fields map[string]interface{}) {
-	l.log(Warning, msg, fields)
-}
-
-// Error отправляет ERROR-сообщение.
-func (l *Logger) Error(msg string, fields map[string]interface{}) {
-	l.log(Error, msg, fields)
-}
-
-// Exception отправляет сообщение об исключении.
-func (l *Logger) Exception(msg string, fields map[string]interface{}) {
-	l.log(Exception, msg, fields)
 }
